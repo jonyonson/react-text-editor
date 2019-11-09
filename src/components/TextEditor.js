d663bf5e1aac4cc1fb95f41a6b8007917c399661
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-11-09 09:54:17 -0600

    first commit

---
+++ src/components/TextEditor.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Editor, EditorState, RichUtils, getDefaultKeyBinding } from 'draft-js';
+import InlineStyleControls from './InlineStyleControls';
+import BlockStyleControls from './BlockStyleControls';
+
+function TextEditor() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const editor = useRef(null);
+
+  function focusEditor() {
+    editor.current.focus();
+  }
+
+  // const styleMap = {
+  //   CODE: {
+  //     backgroundColor: 'rgba(0, 0, 0, 0.05)',
+  //     fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+  //     fontSize: 16,
+  //     padding: 2,
+  //   },
+  // };
+
+  function getBlockStyle(block) {
+    switch (block.getType()) {
+      case 'blockquote':
+        return 'RichEditor-blockquote';
+      default:
+        return null;
+    }
+  }
+
+  function handleChange(editorState) {
+    setEditorState(editorState);
+  }
+
+  function handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      handleChange(newState);
+      return true;
+    }
+    return false;
+  }
+
+  function mapKeyToEditorCommand(e) {
+    if (e.keyCode === 9) {
+      const newEditorState = RichUtils.onTab(e, editorState, 4);
+      if (newEditorState !== editorState) {
+        handleChange(newEditorState);
+      }
+      return;
+    }
+    return getDefaultKeyBinding(e);
+  }
+
+  function toggleBlockType(blockType) {
+    handleChange(RichUtils.toggleBlockType(editorState, blockType));
+  }
+
+  function toggleInlineStyle(inlineStyle) {
+    handleChange(RichUtils.toggleInlineStyle(editorState, inlineStyle));
+  }
+
+  useEffect(() => {
+    focusEditor();
+  }, []);
+
+  let className = 'RichEditor-editor';
+  var contentState = editorState.getCurrentContent();
+  if (!contentState.hasText()) {
+    if (
+      contentState
+        .getBlockMap()
+        .first()
+        .getType() !== 'unstyled'
+    ) {
+      className += ' RichEditor-hidePlaceholder';
+    }
+  }
+
+  return (
+    <div className="RichEditor-root">
+      <BlockStyleControls
+        editorState={editorState}
+        onToggle={toggleBlockType}
+      />
+      <InlineStyleControls
+        editorState={editorState}
+        onToggle={toggleInlineStyle}
+      />
+      <div className={className} onClick={focusEditor}>
+        <Editor
+          blockStyleFn={getBlockStyle}
+          // customStyleMap={styleMap}
+          editorState={editorState}
+          handleKeyCommand={handleKeyCommand}
+          keyBindingFn={mapKeyToEditorCommand}
+          onChange={handleChange}
+          // placeholder="Say something..."
+          ref={editor}
+          spellCheck={true}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default TextEditor;
